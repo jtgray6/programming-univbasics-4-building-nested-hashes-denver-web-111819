8e33a3422089a1e0ad4a4b55c7fd0ba66c27e788
Jeremy Gray <jeremygray@utexas.edu>
2019-11-14 00:42:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_third_tier
 	  {:pieces => 4,
 	  :rent_in_dollars=>{
 	    :one_piece_owned=>25,
-	    :key2=>"t2",
+	    :two_piece_owned=>50,
 	    :key3=>"t3",
 	    :key4=>"t4"
 	  },
