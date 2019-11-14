4c4728a54f74f1243fabe824909f6c01691bc99e
Jeremy Gray <jeremygray@utexas.edu>
2019-11-14 00:45:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,7 @@ def monopoly_with_third_tier
 	  },
 	  :names=>{
 	    :reading_railroad=>{},
-	    :pennsylvania_railroad={},
+	    :pennsylvania_railroad=>{},
 	    :key7=>"t7",
 	    :key8=>"t8"
 	  }
