490c9d148ad4e4eba55bd53c9ea9980ab563ecdd
Jeremy Gray <jeremygray@utexas.edu>
2019-11-14 00:32:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,8 +12,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   {:railroads => 
-	  {:pieces => 4},
-	  {:rent_in_dollars => {}}
+	  {:pieces => 4}
 	}
 end
 
@@ -24,6 +23,7 @@ def monopoly_with_third_tier
   # complexity.
   {:railroads => 
 	  {:pieces => 4},
+	  {:rent_in_dollars=>{}}
 	}
 end
 
